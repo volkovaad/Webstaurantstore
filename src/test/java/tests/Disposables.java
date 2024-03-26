ac93f0dd587f479aa44fdd5d445c19f834ed370b
Elshan Mamedov <elikmtl@gmail.com>
2024-03-26 15:12:01 -0400

    3 tests

--- src/test/java/tests/Disposables.java
+++ src/test/java/tests/Disposables.java
@@ -1,11 +1,17 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.CategoryPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.SeleniumUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Disposables extends TestBase{
     @Test(groups = "regression")
@@ -13,22 +19,37 @@ public class Disposables extends TestBase{
         Driver.getDriver().get(ConfigReader.getProperty("url"));
         SeleniumUtils.jsClick(Driver.getDriver().findElement(By.xpath("//button[text()='Accept']")));
         CategoryPage categoryPage = new CategoryPage();
-        categoryPage.clickCategoryLink("Disposables");
+        categoryPage.clickCategoryLink();
         categoryPage.hoverOverCategoryLink("Disposables");
-        categoryPage.clickCategoryLink("Shop All");
+        categoryPage.clickShopAllLink();
     }
     @Test(groups = "regression")
     public void test2(){
         Driver.getDriver().get(ConfigReader.getProperty("url"));
-//        SeleniumUtils.jsClick(Driver.getDriver().findElement(By.xpath("//button[text()='Accept']")));
+        SeleniumUtils.jsClick(Driver.getDriver().findElement(By.xpath("//button[text()='Accept']")));
         CategoryPage categoryPage = new CategoryPage();
 
-        categoryPage.clickCategoryLink("Disposables");
+        categoryPage.clickCategoryLink();
 
         categoryPage.topProductsQuantity("2");
 
         categoryPage.addToCartTopProduct();
+    }
+    @Test(groups = "regression")
+    public void test3() {
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        SeleniumUtils.jsClick(Driver.getDriver().findElement(By.xpath("//button[text()='Accept']")));
+        CategoryPage categoryPage = new CategoryPage();
 
+        categoryPage.clickCategoryLink();
+
+        Assert.assertEquals(categoryPage.getGlovesDropdownFirstSelectedOptionText(), "Size");
+
+
+        List<String > expectedSizes = List.of("Size", "Small - $19.99", "Medium - $19.99", "Large - $19.99", "Extra Large - $19.99");
+        List<String> actualSizes = categoryPage.getGlovesDropdownOptionsText();
+
+        Assert.assertEquals(actualSizes, expectedSizes);
 
 
 
