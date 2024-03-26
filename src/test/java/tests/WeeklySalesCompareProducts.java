87532cf063d7ab44ddfdea95777cd812a078a13a
Andy Ro <andyroro93@gmail.com>
2024-03-25 21:14:32 -0700

    First commit

---
+++ src/test/java/tests/WeeklySalesCompareProducts.java
@@ -0,0 +1,59 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.WeeklySales;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class WeeklySalesCompareProducts extends TestBase{
+
+
+    @Test(groups = "smoke")
+    public void test() throws InterruptedException {
+
+        logger.info("Weekly Sales Dropdown Rating: High to Low");
+
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+
+        WeeklySales weeklySales = new WeeklySales();
+
+        weeklySales.WeeklySalesPage();
+
+        Thread.sleep(1000);
+
+        Select makeDropdown = new Select(weeklySales.getDropDown());
+
+        Thread.sleep(1000);
+
+        makeDropdown.selectByVisibleText("Rating: High to Low");
+
+        Thread.sleep(1000);
+
+        Assert.assertTrue(Driver.getDriver().getPageSource().contains("Rating: High to Low"));
+
+        Driver.getDriver().findElement(By.partialLinkText("Acopa")).click();
+
+        Thread.sleep(1000);
+
+        Assert.assertTrue(Driver.getDriver().getPageSource().contains("Acopa Manchester 9 Qt. "));
+
+        weeklySales.getCompareProducts().click();
+
+        Thread.sleep(1000);
+
+        weeklySales.getCompareProduct1().click();
+        weeklySales.getCompareProduct2().click();
+        weeklySales.getCompareProductsResult().click();
+
+        Thread.sleep(1000);
+
+        Assert.assertTrue(Driver.getDriver().getPageSource().contains("Compare Products"));
+
+
+
+
+    }
+}
